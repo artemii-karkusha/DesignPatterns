584eb15b5c4801d3808b2397b85f54032f3da2de
Artemii Karkusha <artemii.karkusha@gmail.com>
2022-04-21 07:51:11 +0300

    Created empty module.

---
+++ app/code/Maestro/DesignPatterns/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Artemii Karkusha
+ * @copyright Copyright (c) Artemii Karkusha
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Maestro_DesignPatterns',
+    __DIR__
+);
